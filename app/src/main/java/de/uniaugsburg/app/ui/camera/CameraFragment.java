7ca0306a3586bec071a777cd9ea3239b01fe7300
Bastian <pechlerbastian@googlemail.com>
2023-03-27 17:21:16 +0200

    progress

---
+++ app/src/main/java/de/uniaugsburg/app/ui/camera/CameraFragment.java
@@ -0,0 +1,44 @@
+package de.uniaugsburg.app.ui.camera;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import de.uniaugsburg.app.R;
+import de.uniaugsburg.app.databinding.FragmentCameraBinding;
+import de.uniaugsburg.app.databinding.FragmentDashboardBinding;
+import de.uniaugsburg.app.ui.dashboard.DashboardViewModel;
+
+public class CameraFragment extends Fragment {
+
+    private FragmentCameraBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        CameraViewModel cameraViewModel =
+                new ViewModelProvider(this).get(CameraViewModel.class);
+
+        binding = FragmentCameraBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textCamera;
+        cameraViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
