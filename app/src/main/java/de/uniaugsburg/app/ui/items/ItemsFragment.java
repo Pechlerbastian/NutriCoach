c4747ce32dcae4fe79896dc0648fe9859554af68
Bastian <pechlerbastian@googlemail.com>
2023-03-28 15:04:40 +0200

    added list fragment, dummy varibales and json parser

--- app/src/main/java/de/uniaugsburg/app/ui/items/ItemsFragment.java
+++ app/src/main/java/de/uniaugsburg/app/ui/items/ItemsFragment.java
@@ -4,17 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import org.json.JSONException;
+import java.util.ArrayList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import de.uniaugsburg.app.R;
 import de.uniaugsburg.app.databinding.FragmentItemsBinding;
+
+import de.uniaugsburg.app.util.JsonParser;
+
 
 public class ItemsFragment extends Fragment {
 
     private FragmentItemsBinding binding;
+    private ListView listView;
+
+    List<Map<String, String>> dataList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +40,52 @@ public class ItemsFragment extends Fragment {
         binding = FragmentItemsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        listView = root.findViewById(R.id.list_view);
+
+        updateData(true);
+
         return root;
+    }
+
+    private void updateData(boolean fromAsset) {
+        Map<String, List<Integer>> itemKcalMap;
+        if(fromAsset) {
+            itemKcalMap = JsonParser.parseJsonFromAsset(this.requireContext());
+        }else {
+            itemKcalMap = JsonParser.parseJson(this.requireContext());
+        }
+        // Create the list adapter
+        dataList = new ArrayList<>();
+        for (String key : itemKcalMap.keySet()) {
+            Map<String, String> dataMap = new HashMap<>();
+            dataMap.put("item_name", key);
+            dataMap.put("item_kcal", (Objects.requireNonNull(itemKcalMap.get(key)).get(0) + " kcal"));
+            dataList.add(dataMap);
+        }
+        SimpleAdapter adapter = new SimpleAdapter(this.getContext(), dataList,
+                android.R.layout.simple_list_item_2,
+                new String[] {"item_name", "item_kcal"},
+                new int[] {android.R.id.text1, android.R.id.text2});
+
+        // Set the list adapter
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onResume() {
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        Map<String, List<Integer>> oldMap = JsonParser.parseJsonFromAsset(this.requireContext());
+        oldMap.put("item99", list);
+
+        try {
+            JsonParser.writeJson(oldMap, this.requireContext());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        updateData(false);
+        super.onResume();
     }
 
     @Override
@@ -34,4 +93,6 @@ public class ItemsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+
 }
---
+++ app/src/main/java/de/uniaugsburg/app/util/JsonParser.java
@@ -0,0 +1,130 @@
+package de.uniaugsburg.app.util;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class JsonParser {
+
+
+    public static Map<String, List<Integer>> parseJson(Context context) {
+        StringBuilder jsonString = new StringBuilder();
+        try {
+            FileInputStream inputStream = context.openFileInput("items.json");
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader((inputStream)));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                jsonString.append(line);
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new HashMap<>();
+        }
+
+        // Parse the JSON string
+        Map<String, List<Integer>> itemKcalMap = null;
+        try {
+            JSONObject json = new JSONObject(jsonString.toString());
+            itemKcalMap = new HashMap<>();
+
+            // Iterate over the JSON keys and add them to the map
+            Iterator<String> keys = json.keys();
+            while (keys.hasNext()) {
+                String key = keys.next();
+                JSONObject item = json.getJSONObject(key);
+                int kcal = item.getInt("kcal");
+
+                if (!itemKcalMap.containsKey(key)) {
+                    itemKcalMap.put(key, new ArrayList<>());
+                    Objects.requireNonNull(itemKcalMap.get(key)).add(kcal);
+                }
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return itemKcalMap;
+    }
+
+    public static Map<String, List<Integer>> parseJsonFromAsset(Context context) {
+        StringBuilder jsonString = new StringBuilder();
+        try {
+            // TODO remove and use the internal storage! Assets are read only
+            AssetManager assetManager = context.getAssets();
+            InputStream inputStream = assetManager.open("list_values.json");
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader((inputStream)));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                jsonString.append(line);
+            }
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new HashMap<>();
+        }
+
+        // Parse the JSON string
+        Map<String, List<Integer>> itemKcalMap = null;
+        try {
+            JSONObject json = new JSONObject(jsonString.toString());
+            itemKcalMap = new HashMap<>();
+
+            // Iterate over the JSON keys and add them to the map
+            Iterator<String> keys = json.keys();
+            while (keys.hasNext()) {
+                String key = keys.next();
+                JSONObject item = json.getJSONObject(key);
+                int kcal = item.getInt("kcal");
+
+                if (!itemKcalMap.containsKey(key)) {
+                    itemKcalMap.put(key, new ArrayList<>());
+                    Objects.requireNonNull(itemKcalMap.get(key)).add(kcal);
+                }
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return itemKcalMap;
+    }
+
+    public static void writeJson(Map<String, List<Integer>> itemKcalMap, Context context)
+            throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        for (String key : itemKcalMap.keySet()) {
+            JSONObject jsonObject2 = new JSONObject();
+            jsonObject2.put("kcal", Objects.requireNonNull(itemKcalMap.get(key)).get(0));
+            jsonObject.put(key, jsonObject2);
+        }
+
+        String jsonString = jsonObject.toString();
+
+        try {
+            FileOutputStream outputStream = context.openFileOutput("items.json",
+                    Context.MODE_PRIVATE);
+            outputStream.write(jsonString.getBytes());
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
