f6746cc93c24ac286514753c6a74ac2180e71a24
hubanton <anton.huber98@yahoo.com>
2023-03-28 15:24:30 +0200

    add dummy api

--- app/src/main/java/de/uniaugsburg/app/ui/add/AddViewModel.java
+++ app/src/main/java/de/uniaugsburg/app/ui/add/AddViewModel.java
@@ -4,6 +4,14 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class AddViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
@@ -13,7 +21,22 @@ public class AddViewModel extends ViewModel {
     }
 
     public void changeValue() {
-        mText.setValue("new");
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("dummy.com")
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                mText.setValue("failure");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                mText.setValue("beans");
+            }
+        });
     }
 
     public LiveData<String> getText() {
