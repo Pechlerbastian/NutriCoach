889096532e183acb761c6aff42b4ffbcf4e3fa63
Willi Schiebelbein <willi.schiebelbein@gmx.de>
2023-03-28 13:16:57 +0200

    Add functionality to take a photo

--- app/src/main/java/de/uniaugsburg/app/ui/camera/CameraFragment.java
+++ app/src/main/java/de/uniaugsburg/app/ui/camera/CameraFragment.java
@@ -1,22 +1,32 @@
 package de.uniaugsburg.app.ui.camera;
 
+import static android.app.Activity.RESULT_OK;
+
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import de.uniaugsburg.app.R;
 import de.uniaugsburg.app.databinding.FragmentCameraBinding;
 
 public class CameraFragment extends Fragment {
 
     private FragmentCameraBinding binding;
+    private View root;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,10 +34,14 @@ public class CameraFragment extends Fragment {
                 new ViewModelProvider(this).get(CameraViewModel.class);
 
         binding = FragmentCameraBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        root = binding.getRoot();
 
-        final TextView textView = binding.textCamera;
-        cameraViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        // Launch camera intent to take a photo
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
         return root;
     }
 
@@ -37,4 +51,17 @@ public class CameraFragment extends Fragment {
         binding = null;
     }
 
+    // receive photo taken and give ml model for prediction TODO
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // Handle photo data
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            // rework from here
+            ImageView imageView = root.findViewById(R.id.image_view);
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
 }
